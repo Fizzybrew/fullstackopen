97a71fc9177ae03ab0fbba643a07aff38b58286a
Sasha200852 <utkinalexander28@gmail.com>
2026-04-12 11:23:07 +0300

    done 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,46 @@
 import { useState } from "react";
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-  if(all === 0) return <p>No feedback given</p>
+  if (all === 0) return <p>No feedback given</p>;
   return (
     <div>
       <h2>Statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
     </div>
   );
 };
 
-const Button = ({text, handleClick}) => 
-  <button onClick={handleClick}>
-    {text}
-  </button>
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const StatisticLine = ({text, value}) => 
-  <p>{text}: {value} {text === "positive" ? "%" : ""}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>
+        {value} {text === "positive" ? "%" : ""}
+      </td>
+    </tr>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const all = good + neutral + bad;
-  const average = all === 0 ? 0 : (good - bad) / all;
-  const positive = all === 0 ? 0 : (good / all) * 100;
+  const average = all === 0 ? 0 : ((good - bad) / all).toFixed(2);
+  const positive = all === 0 ? 0 : ((good / all) * 100).toFixed(2);
 
   return (
     <div>
