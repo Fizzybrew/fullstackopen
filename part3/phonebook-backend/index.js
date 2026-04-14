e5b04a2012b70802c0208e9c07ba10a94eb0301c
Sasha200852 <utkinalexander28@gmail.com>
2026-04-14 11:05:46 +0300

    done 3.7

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,15 +1,10 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-
-morgan.token('body', (request) => {
-  return JSON.stringify(request.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
   {
