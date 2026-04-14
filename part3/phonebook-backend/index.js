87ef994d0157958db25ebefaab6713c84e0bc773
Sasha200852 <utkinalexander28@gmail.com>
2026-04-14 09:39:21 +0300

    done 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,30 +3,40 @@ const app = express();
 
 
 let persons = [
-  { 
+  {
     "id": "1",
-    "name": "Arto Hellas", 
+    "name": "Arto Hellas",
     "number": "040-123456"
   },
-  { 
+  {
     "id": "2",
-    "name": "Ada Lovelace", 
+    "name": "Ada Lovelace",
     "number": "39-44-5323523"
   },
-  { 
+  {
     "id": "3",
-    "name": "Dan Abramov", 
+    "name": "Dan Abramov",
     "number": "12-43-234345"
   },
-  { 
+  {
     "id": "4",
-    "name": "Mary Poppendieck", 
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122"
   }
 ];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+  const now = new Date();
+  response.send(
+    `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${now}</p>
+    `
+  );
 });
 
 const PORT = 3001
